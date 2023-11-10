d6e216818d3336f08cfe8e200ee0dde981e5f76e
Guido Pereira <stereopt@gmail.com>
2023-11-10 22:51:31 +0000

    feat: clean tree

--- src/store/index.ts
+++ src/store/index.ts
@@ -2,27 +2,9 @@ import { applyNodeChanges, applyEdgeChanges } from 'reactflow';
 import { nanoid } from 'nanoid';
 import { create } from 'zustand';
 
-const add = (value: number) => {
-  return Number(value) + Number(value);
-};
-
 const useStore = create((set, get) => ({
-  nodes: [
-    { id: '1', position: { x: 0, y: 0 }, data: { input: '2' }, type: 'input' },
-    {
-      id: '2',
-      position: { x: 0, y: 100 },
-      data: { action: add },
-      type: 'default',
-    },
-    {
-      id: '3',
-      position: { x: 0, y: 200 },
-      data: { label: '' },
-      type: 'output',
-    },
-  ],
-  edges: [{ id: 'e1-2', source: '1', target: '2' }],
+  nodes: [],
+  edges: [],
 
   addNode({ type = 'default', label }: { type: string; label: string }) {
     const id = nanoid();
