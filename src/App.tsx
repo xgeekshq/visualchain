13a4b61a7e3a01eeefd7eb1007a880a565551565
Guido Pereira <stereopt@gmail.com>
2023-11-11 22:09:15 +0000

    fix: reactflow reference

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,9 @@
-import ReactFlow, { Background, Controls, MiniMap } from 'reactflow';
+import ReactFlow, {
+  Background,
+  Controls,
+  MiniMap,
+  ReactFlowProvider,
+} from 'reactflow';
 import { shallow } from 'zustand/shallow';
 import useStore from './store';
 
@@ -17,208 +22,212 @@ import { useCallback, useRef, useState } from 'react';
 import OpenAITranscription from './nodes/OpenAITranscription';
 
 const nodeTypes = {
-	openAI: OpenAI,
-	openAICompletion: OpenAICompletion,
-	openAIImages: OpenAIImages,
-	openAITranscription: OpenAITranscription,
-	end: End,
-	start: Start,
-	codeBlock: CodeBlock,
-	explanation: Explanation,
-	codeLanguage: CodeTypeSelector,
+  openAI: OpenAI,
+  openAICompletion: OpenAICompletion,
+  openAIImages: OpenAIImages,
+  openAITranscription: OpenAITranscription,
+  end: End,
+  start: Start,
+  codeBlock: CodeBlock,
+  explanation: Explanation,
+  codeLanguage: CodeTypeSelector,
 };
 
 const selector = (store) => ({
-	nodes: store.nodes,
-	edges: store.edges,
-	data: store.data,
-	language: store.language,
-	onNodesChange: store.onNodesChange,
-	onEdgesChange: store.onEdgesChange,
-	createNode: store.createNode,
-	addEdge: store.addEdge,
-	updateNode: store.updateNode,
-	appendFlow: store.appendFlow,
-	getNode: store.getNode,
-	addNode: store.addNode,
-	updateData: store.updateData,
-	updateLanguage: store.updateLanguage
+  nodes: store.nodes,
+  edges: store.edges,
+  data: store.data,
+  language: store.language,
+  onNodesChange: store.onNodesChange,
+  onEdgesChange: store.onEdgesChange,
+  createNode: store.createNode,
+  addEdge: store.addEdge,
+  updateNode: store.updateNode,
+  appendFlow: store.appendFlow,
+  getNode: store.getNode,
+  addNode: store.addNode,
+  updateData: store.updateData,
+  updateLanguage: store.updateLanguage,
 });
 
 export default function App() {
-	const reactFlowWrapper = useRef(null);
-	const store = useStore(selector, shallow);
-	const [reactFlowInstance, setReactFlowInstance] = useState(null);
+  const reactFlowWrapper = useRef(null);
+  const store = useStore(selector, shallow);
+  const [reactFlowInstance, setReactFlowInstance] = useState(null);
 
-	const onDragOver = useCallback((event) => {
-		event.preventDefault();
-		event.dataTransfer.dropEffect = 'move';
-	}, []);
+  const onDragOver = useCallback((event) => {
+    event.preventDefault();
+    event.dataTransfer.dropEffect = 'move';
+  }, []);
 
-	const onDrop = useCallback(
-		(event) => {
-			event.preventDefault();
-			const type = event.dataTransfer.getData('application/reactflow');
-			if (typeof type === 'undefined' || !type) {
-				return;
-			}
+  const onDrop = useCallback(
+    (event) => {
+      event.preventDefault();
+      const type = event.dataTransfer.getData('application/reactflow');
+      if (typeof type === 'undefined' || !type) {
+        return;
+      }
 
-			const position = reactFlowInstance.screenToFlowPosition({
-				x: event.clientX,
-				y: event.clientY,
-			});
+      const position = reactFlowInstance.screenToFlowPosition({
+        x: event.clientX,
+        y: event.clientY,
+      });
 
-			store.createNode(type, position);
-		},
-		[reactFlowInstance],
-	);
+      store.createNode(type, position);
+    },
+    [reactFlowInstance],
+  );
 
-	const isValidConnection = (connection) => {
-		const sourceNode = store.nodes.find((val) => val.id === connection.source);
+  const isValidConnection = (connection) => {
+    const sourceNode = store.nodes.find((val) => val.id === connection.source);
 
-		const targetNode = store.nodes.find((val) => val.id === connection.target);
-		console.log(sourceNode.type, targetNode.type)
-		if (sourceNode.type === 'start' && targetNode.type === 'openAI') {
-			return true;
-		}
+    const targetNode = store.nodes.find((val) => val.id === connection.target);
+    console.log(sourceNode.type, targetNode.type);
+    if (sourceNode.type === 'start' && targetNode.type === 'openAI') {
+      return true;
+    }
 
-		if (
-			sourceNode.type === 'openAI' &&
-			(targetNode.type === 'openAICompletion' ||
-				targetNode.type === 'openAIImages' ||
-				targetNode.type === 'openAITranscription')
-		) {
-			return true;
-		}
+    if (
+      sourceNode.type === 'openAI' &&
+      (targetNode.type === 'openAICompletion' ||
+        targetNode.type === 'openAIImages' ||
+        targetNode.type === 'openAITranscription')
+    ) {
+      return true;
+    }
 
-		if (
-			(sourceNode.type === 'openAICompletion' ||
-				sourceNode.type === 'openAIImages' ||
-				sourceNode.type === 'openAITranscription') &&
-			targetNode.type === 'codeLanguage'
-		) {
-			return true;
-		}
+    if (
+      (sourceNode.type === 'openAICompletion' ||
+        sourceNode.type === 'openAIImages' ||
+        sourceNode.type === 'openAITranscription') &&
+      targetNode.type === 'codeLanguage'
+    ) {
+      return true;
+    }
 
-		if (sourceNode.type === 'codeLanguage' && targetNode.type === 'end') {
-			return true;
-		}
+    if (sourceNode.type === 'codeLanguage' && targetNode.type === 'end') {
+      return true;
+    }
 
-		return false;
-	};
+    return false;
+  };
 
-	function getAllFlows(startNodeId, currentPath = [], allPaths = []) {
-		const currentNode = store.nodes.find((node) => node.id === startNodeId);
+  function getAllFlows(startNodeId, currentPath = [], allPaths = []) {
+    const currentNode = store.nodes.find((node) => node.id === startNodeId);
 
-		// If the current node is a "stop" type, add the current path to the list of all paths
-		if (currentNode.type === 'end') {
-			allPaths.push([...currentPath, currentNode.id]);
-			return;
-		}
+    // If the current node is a "stop" type, add the current path to the list of all paths
+    if (currentNode.type === 'end') {
+      allPaths.push([...currentPath, currentNode.id]);
+      return;
+    }
 
-		// Iterate through outgoing edges from the current node
-		const outgoingEdges = store.edges.filter(
-			(edge) => edge.source === startNodeId,
-		);
-		for (const edge of outgoingEdges) {
-			const nextNodeId = edge.target;
+    // Iterate through outgoing edges from the current node
+    const outgoingEdges = store.edges.filter(
+      (edge) => edge.source === startNodeId,
+    );
+    for (const edge of outgoingEdges) {
+      const nextNodeId = edge.target;
 
-			// Avoid cycles by checking if the next node is already in the current path
-			if (!currentPath.includes(nextNodeId)) {
-				// Recursively explore the next node
-				getAllFlows(nextNodeId, [...currentPath, currentNode.id], allPaths);
-			}
-		}
-	}
+      // Avoid cycles by checking if the next node is already in the current path
+      if (!currentPath.includes(nextNodeId)) {
+        // Recursively explore the next node
+        getAllFlows(nextNodeId, [...currentPath, currentNode.id], allPaths);
+      }
+    }
+  }
 
-	const handleRun = () => {
-		const myJson = {};
-		const startNode = store.nodes.find((val) => val.type === 'start');
-		if (!startNode) return;
+  const handleRun = () => {
+    const myJson = {};
+    const startNode = store.nodes.find((val) => val.type === 'start');
+    if (!startNode) return;
 
-		const allPaths = [];
-		getAllFlows(startNode.id, [], allPaths);
+    const allPaths = [];
+    getAllFlows(startNode.id, [], allPaths);
 
-		if (allPaths.length <= 0) return;
+    if (allPaths.length <= 0) return;
 
-		for (const nodes of allPaths) {
-			for (let i = 0; i < nodes.length; i++) {
-				const node = store.getNode(nodes[i]);
-				console.log(node);
-				myJson[node.type] = node.data;
-			}
-		}
+    for (const nodes of allPaths) {
+      for (let i = 0; i < nodes.length; i++) {
+        const node = store.getNode(nodes[i]);
+        console.log(node);
+        myJson[node.type] = node.data;
+      }
+    }
 
-		let myCode = "";
-		console.log(store.language)
-		for (const x in myJson) {
-			if (x === "start" || x === "end" || x === "codeLanguage") continue;
+    let myCode = '';
+    console.log(store.language);
+    for (const x in myJson) {
+      if (x === 'start' || x === 'end' || x === 'codeLanguage') continue;
 
-			const { fn, ...args } = myJson[x];
+      const { fn, ...args } = myJson[x];
 
-			if (fn) {
-				myCode += fn({ ...args, language: store.language });
-				myCode += `
+      if (fn) {
+        myCode += fn({ ...args, language: store.language });
+        myCode += `
 `;
-			}
-		}
+      }
+    }
 
-		// console.log(myCode);
-		store.updateData(myCode);
-		store.createNode('codeBlock');
-	};
+    // console.log(myCode);
+    store.updateData(myCode);
+    store.createNode('codeBlock');
+  };
 
-	return (
-		<div className="flex w-full h-screen">
-			<Navbar>
-				<NavbarItem label="Run" onClick={() => handleRun()} />
-				<NavbarItem
-					label="OpenAI key"
-					type="openAI"
-					onClick={() => store.createNode('openAI')}
-					draggable
-				/>
-				<NavbarItem
-					label="OpenAICompletion"
-					type="openAICompletion"
-					onClick={() => store.createNode('openAICompletion')}
-					draggable
-				/>
-				<NavbarItem
-					label="OpenAIImages"
-					type="openAIImages"
-					onClick={() => store.createNode('openAIImages')}
-					draggable
-				/>
-				<NavbarItem
-					label="OpenAITranscription"
-					type="openAITranscription"
-					onClick={() => store.createNode('openAITranscription')}
-					draggable
-				/>
-				<NavbarItem
-					label="Code language"
-					type="codeLanguage"
-					onClick={() => store.createNode('codeLanguage')}
-					draggable
-				/>
-			</Navbar>
-			<ReactFlow
-				nodes={store.nodes}
-				edges={store.edges}
-				nodeTypes={nodeTypes}
-				onNodesChange={store.onNodesChange}
-				onEdgesChange={store.onEdgesChange}
-				onConnect={store.addEdge}
-				isValidConnection={isValidConnection}
-				onInit={setReactFlowInstance}
-				onDrop={onDrop}
-				onDragOver={onDragOver}
-				fitView>
-				<Controls />
-				<MiniMap />
-				<Background variant="dots" gap={12} size={1} />
-			</ReactFlow>
-		</div>
-	);
+  return (
+    <div className="flex w-full h-screen">
+      <Navbar>
+        <NavbarItem label="Run" onClick={() => handleRun()} />
+        <NavbarItem
+          label="OpenAI key"
+          type="openAI"
+          onClick={() => store.createNode('openAI')}
+          draggable
+        />
+        <NavbarItem
+          label="OpenAICompletion"
+          type="openAICompletion"
+          onClick={() => store.createNode('openAICompletion')}
+          draggable
+        />
+        <NavbarItem
+          label="OpenAIImages"
+          type="openAIImages"
+          onClick={() => store.createNode('openAIImages')}
+          draggable
+        />
+        <NavbarItem
+          label="OpenAITranscription"
+          type="openAITranscription"
+          onClick={() => store.createNode('openAITranscription')}
+          draggable
+        />
+        <NavbarItem
+          label="Code language"
+          type="codeLanguage"
+          onClick={() => store.createNode('codeLanguage')}
+          draggable
+        />
+      </Navbar>
+      <ReactFlowProvider>
+        <div className="flex-grow h-full" ref={reactFlowWrapper}>
+          <ReactFlow
+            nodes={store.nodes}
+            edges={store.edges}
+            nodeTypes={nodeTypes}
+            onNodesChange={store.onNodesChange}
+            onEdgesChange={store.onEdgesChange}
+            onConnect={store.addEdge}
+            isValidConnection={isValidConnection}
+            onInit={setReactFlowInstance}
+            onDrop={onDrop}
+            onDragOver={onDragOver}
+            fitView>
+            <Controls />
+            <MiniMap />
+            <Background variant="dots" gap={12} size={1} />
+          </ReactFlow>
+        </div>
+      </ReactFlowProvider>
+    </div>
+  );
 }
--- src/store/index.ts
+++ src/store/index.ts
@@ -8,138 +8,142 @@ import {
   openAITranscription,
 } from '../codeSnippets/general';
 
-const useStore = create((set, get) => ({
-  nodes: [
-    { id: nanoid(), type: 'start', position: { x: 50, y: 50 } },
-    { id: nanoid(), type: 'end', position: { x: 50, y: 700 } },
-  ],
-  edges: [],
-  data: {},
-  explanation: '',
-  language: 'js',
-  getNode(id: string) {
-    return get().nodes.find((node) => node.id === id);
-  },
+const useStore = create((set, get) => {
+  return {
+    nodes: [
+      { id: nanoid(), type: 'start', position: { x: 50, y: 50 } },
+      { id: nanoid(), type: 'end', position: { x: 50, y: 700 } },
+    ],
+    edges: [],
+    data: {},
+    explanation: '',
+    language: 'js',
 
-  addNode(data: object, type: string) {
-    const id = nanoid();
+    getNode(id: string) {
+      return get().nodes.find((node) => node.id === id);
+    },
 
-    const newNode = {
-      id,
-      position: { x: 0, y: 0 },
-      data,
-      type,
-    };
-    set({ nodes: [...get().nodes, newNode] });
-  },
+    addNode(data: object, type: string) {
+      const id = nanoid();
 
-  onNodesChange(changes) {
-    const removeNode = changes.find((c) => c.type === 'remove');
-    if (removeNode) {
-      const nodeToRemove = get().nodes.find((n) => n.id === removeNode.id);
+      const newNode = {
+        id,
+        position: { x: 0, y: 0 },
+        data,
+        type,
+      };
+      set({ nodes: [...get().nodes, newNode] });
+    },
 
-      if (nodeToRemove.type === 'start' || nodeToRemove.type === 'end') return;
-    }
+    onNodesChange(changes) {
+      const removeNode = changes.find((c) => c.type === 'remove');
+      if (removeNode) {
+        const nodeToRemove = get().nodes.find((n) => n.id === removeNode.id);
 
-    set({
-      nodes: applyNodeChanges(changes, get().nodes),
-    });
-  },
-
-  onEdgesChange(changes) {
-    set({
-      edges: applyEdgeChanges(changes, get().edges),
-    });
-  },
-
-  updateNode(id, data) {
-    set({
-      nodes: get().nodes.map((node) =>
-        node.id === id ? { ...node, data: { ...node.data, ...data } } : node
-      ),
-    });
-  },
-
-  addEdge(data) {
-    const id = nanoid(6);
-    const edge = { id, ...data };
-
-    set({ edges: [edge, ...get().edges] });
-  },
-
-  createNode(type, position = { x: 0, y: 0 }) {
-    const id = nanoid();
-
-    switch (type) {
-      case 'openAI': {
-        const data = { apiKey: '', fn: initOpenAI };
-        set({ nodes: [...get().nodes, { id, type, data, position }] });
-        break;
+        if (nodeToRemove.type === 'start' || nodeToRemove.type === 'end')
+          return;
       }
-      case 'openAICompletion': {
-        const data = {
-          model: 'gpt-3.5-turbo-1106',
-          temperature: 0,
-          prompt: '',
-          fn: openAICompletion,
-        };
-        set({ nodes: [...get().nodes, { id, type, data, position }] });
-        break;
-      }
-      case 'openAIImages': {
-        const data = {
-          prompt: '',
-          numImages: 1,
-          height: 512,
-          width: 512,
-          fn: openAIImageCompletion,
-        };
-        set({ nodes: [...get().nodes, { id, type, data, position }] });
-        break;
-      }
-      case 'start': {
-        set({ nodes: [...get().nodes, { id, type, position }] });
-        break;
-      }
-      case 'end': {
-        set({ nodes: [...get().nodes, { id, type, position }] });
-        break;
-      }
-      case 'codeBlock': {
-        const data = { code: get().data };
-        const position = { x: 200, y: 300 };
-        set({ nodes: [...get().nodes, { id, type, data, position }] });
-        break;
-      }
-      case 'explanation': {
-        const data = { code: get().data };
-        const position = { x: 100, y: 100 };
-        set({ nodes: [...get().nodes, { id, type, data, position }] });
-        break;
-      }
-      case 'codeLanguage': {
-        const data = { language: 'js' };
-        set({ nodes: [...get().nodes, { id, type, data, position }] });
-        break;
-      }
-      case 'openAITranscription': {
-        const data = { filePath: '', fn: openAITranscription };
-        set({ nodes: [...get().nodes, { id, type, data, position }] });
-        break;
-      }
-    }
-  },
 
-  updateData(newData) {
-    set({ data: newData });
-  },
+      set({
+        nodes: applyNodeChanges(changes, get().nodes),
+      });
+    },
 
-  updateExplanation(newExplanation) {
-    set({ explanation: get().explanation.concat(newExplanation) });
-  },
-  updateLanguage(newLanguage) {
-    set({ language: newLanguage });
-  },
-}));
+    onEdgesChange(changes) {
+      set({
+        edges: applyEdgeChanges(changes, get().edges),
+      });
+    },
+
+    updateNode(id, data) {
+      set({
+        nodes: get().nodes.map((node) =>
+          node.id === id ? { ...node, data: { ...node.data, ...data } } : node,
+        ),
+      });
+    },
+
+    addEdge(data) {
+      const id = nanoid(6);
+      const edge = { id, ...data };
+
+      set({ edges: [edge, ...get().edges] });
+    },
+
+    createNode(type, position = { x: 0, y: 0 }) {
+      const id = nanoid();
+
+      switch (type) {
+        case 'openAI': {
+          const data = { apiKey: '', fn: initOpenAI };
+          set({ nodes: [...get().nodes, { id, type, data, position }] });
+          break;
+        }
+        case 'openAICompletion': {
+          const data = {
+            model: 'gpt-3.5-turbo-1106',
+            temperature: 0,
+            prompt: '',
+            fn: openAICompletion,
+          };
+          set({ nodes: [...get().nodes, { id, type, data, position }] });
+          break;
+        }
+        case 'openAIImages': {
+          const data = {
+            prompt: '',
+            numImages: 1,
+            height: 512,
+            width: 512,
+            fn: openAIImageCompletion,
+          };
+          set({ nodes: [...get().nodes, { id, type, data, position }] });
+          break;
+        }
+        case 'start': {
+          set({ nodes: [...get().nodes, { id, type, position }] });
+          break;
+        }
+        case 'end': {
+          set({ nodes: [...get().nodes, { id, type, position }] });
+          break;
+        }
+        case 'codeBlock': {
+          const data = { code: get().data };
+          const position = { x: 200, y: 300 };
+          set({ nodes: [...get().nodes, { id, type, data, position }] });
+          break;
+        }
+        case 'explanation': {
+          const data = { code: get().data };
+          const position = { x: 100, y: 100 };
+          set({ nodes: [...get().nodes, { id, type, data, position }] });
+          break;
+        }
+        case 'codeLanguage': {
+          const data = { language: 'js' };
+          set({ nodes: [...get().nodes, { id, type, data, position }] });
+          break;
+        }
+        case 'openAITranscription': {
+          const data = { filePath: '', fn: openAITranscription };
+          set({ nodes: [...get().nodes, { id, type, data, position }] });
+          break;
+        }
+      }
+    },
+
+    updateData(newData) {
+      set({ data: newData });
+    },
+
+    updateExplanation(newExplanation) {
+      set({ explanation: get().explanation.concat(newExplanation) });
+    },
+    updateLanguage(newLanguage) {
+      set({ language: newLanguage });
+    },
+  };
+});
 
 export default useStore;
