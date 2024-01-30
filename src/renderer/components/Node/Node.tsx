b576a4cee1e8b1e04b451135fc97dde4fe32f32e
Guido Pereira <stereopt@gmail.com>
2024-01-30 14:11:04 +0000

    feat: all nodes

--- src/renderer/components/Node/Node.tsx
+++ src/renderer/components/Node/Node.tsx
@@ -2,13 +2,23 @@ import React from 'react';
 
 type NodeType = {
   title: string;
-  titleBG?: 'green' | 'blue' | 'yellow' | 'red';
+  titleBG?: 'green' | 'blue' | 'yellow' | 'red' | 'gray' | 'purple';
+  size?: 'md' | 'auto';
   children: React.ReactNode;
 };
 
-const Node = ({ title, titleBG = 'green', children }: NodeType) => {
+const Node = ({
+  title,
+  titleBG = 'green',
+  size = 'md',
+  children,
+}: NodeType) => {
   return (
-    <div className="rounded-md bg-white shadow-xl border w-96">
+    <div
+      className={`rounded-md bg-white shadow-xl border ${
+        size === 'md' ? 'w-96' : ''
+      }`}
+    >
       <p
         className={`rounded-t-md px-2 py-1 bg-${titleBG}-500 text-white text-sm`}
       >
--- src/renderer/nodes/Explanation.tsx
+++ src/renderer/nodes/Explanation.tsx
@@ -1,21 +1,17 @@
-import React from 'react';
 import { shallow } from 'zustand/shallow';
 import useStore from '../store';
+import Node from '../components/Node/Node';
 
 const selector = (store) => ({
   explanation: store.explanation,
 });
+
 export default function Explanation() {
   const store = useStore(selector, shallow);
 
   return (
-    <div className="flex flex-col rounded-md bg-white shadow-xl border w-[600px] z-[999999]">
-      <p className={'rounded-t-md px-2 py-1 bg-purple-500 text-white text-sm'}>
-        Code Explanation
-      </p>
-      <label className="flex px-2 py-1 w-full items-center justify-center">
-        <p className=" text-lg font-bold">{store.explanation}</p>
-      </label>
-    </div>
+    <Node title="Code Explanation" titleBG="purple" size="auto">
+      <p className=" text-lg font-bold">{store.explanation}</p>
+    </Node>
   );
 }
